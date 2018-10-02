fe2f95ba5febc3344449ab2af487f76b6d992fbb
zecollokaris <collo.kariss@gmail.com>
2018-10-02 10:16:31 +0300

    deleted Dist

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,28 +1,30 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
-import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProfileService {
 
   private username: string;
-  private clientid = '203dd913322b3db7d987'
-  private clientsecret = '020c0b3665296b33d758edeeced30473ef44f7da'
+  private clientid = '203dd913322b3db7d987';
+  private clientsecret = '020c0b3665296b33d758edeeced30473ef44f7da';
 
 
   constructor(private http: Http) {
-    console.log("service is now ready");
-    this.username = 'zecollokaris'
+    console.log('service is now ready');
+    this.username = 'zecollokaris';
   }
 
   getProfileInfo() {
-    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+    // tslint:disable-next-line:max-line-length
+    return this.http.get('https://api.github.com/users/' + this.username + '?client_id=' + this.clientid + '&client_secret=' + this.clientsecret)
       .map(res => res.json());
 
   }
 
   getProfileRepos() {
-    return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+    // tslint:disable-next-line:max-line-length
+    return this.http.get('https://api.github.com/users/' + this.username + '/repos?client_id=' + this.clientid + '&client_secret=' + this.clientsecret)
       .map(res => res.json());
   }
 
