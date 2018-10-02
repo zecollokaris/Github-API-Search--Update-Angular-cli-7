944561a79acbd359ebf1d3b2708ab61baebac5ad
zecollokaris <collo.kariss@gmail.com>
2018-10-02 09:41:15 +0300

    project done for now

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -4,7 +4,7 @@ import { ProfileService } from '../services/profile.service';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
   profile: any[];
